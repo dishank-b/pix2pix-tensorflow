e1307e0a4f084cdc059b80aef75a0209f2706d8e
dishank-b <dishankbansal09@gmail.com>
2018-06-25 16:51:02 +0530

    model training well

---
+++ models.py
@@ -0,0 +1,403 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import tensorflow as tf
+import numpy as np
+import cv2
+from ops import *
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from utils import *
+import glob
+
+class my_net(object):
+	"""Single Image Dehazing via Multi-Scale Convolutional Neural Networks"""
+	def __init__(self, model_path):
+		self.model_path = model_path
+		self.graph_path = model_path+"/tf_graph/"
+		self.save_path = model_path + "/saved_model/"
+		self.output_path = model_path + "/results/"
+		if not os.path.exists(model_path):
+			os.makedirs(self.graph_path+"train/")
+			os.makedirs(self.graph_path+"val/")
+		
+	def _debug_info(self):
+		variables_names = [[v.name, v.get_shape().as_list()] for v in tf.trainable_variables()]
+		print "Trainable Variables:"
+		tot_params = 0
+		for i in variables_names:
+			var_params = np.prod(np.array(i[1]))
+			tot_params += var_params
+			print i[0], i[1], var_params
+		print "Total number of Trainable Parameters: ", str(tot_params/1000.0)+"K"
+
+	def build_model(self):
+		with tf.name_scope("Inputs") as scope:
+			self.haze_in = tf.placeholder(tf.float32, shape=[None,240,320,3], name="Haze_Image")
+			self.clear_in = tf.placeholder(tf.float32, shape=[None,240,320,3], name="Clear_Image")
+			self.trans_in = tf.placeholder(tf.float32, shape=[None,240,320,1], name="Trans_Map")
+			self.train_phase = tf.placeholder(tf.bool, name="is_training")
+			hazy_summ = tf.summary.image("Hazy_image", self.haze_in)
+			clear_summ = tf.summary.image("clear_in", self.clear_in)
+
+		with tf.name_scope("Model") as scope:
+			
+			conv1 = Conv_2D(self.haze_in, output_chan=3, kernel=[5,5], stride=[1,1], padding="SAME", name="Conv1")
+			conv2 = Conv_2D(conv1, output_chan=3, kernel=[3,3], stride=[1,1], padding="SAME", name="Conv2")
+			pool1 = max_pool(conv2, name="pool1")
+			unpool1 = max_unpool(pool1, name="unpool1")
+			concat_1 = tf.concat([conv1, unpool1], axis=3, name="Concat_1")
+			self.transMap = Conv_2D(concat_1, output_chan=1, kernel=[3,3], stride=[1,1], padding="SAME", name="TMap")
+			conv4 = Conv_2D(self.haze_in, output_chan=3, kernel=[3,3], stride=[1,1], padding="SAME", name="Conv4")
+			concat_2 = tf.concat([conv4, self.transMap], axis=3, name="Concat_2")
+			conv5 = Conv_2D(concat_2, output_chan=3, kernel=[7,7], stride=[1,1], padding="SAME", name="IandT")
+			concat_3 = tf.concat([self.haze_in, conv5], axis=3, name="Concat_3")
+			self.clearImage = Conv_2D(concat_3, output_chan=3, kernel=[3,3], stride=[1,1], padding="SAME", name="clearImage")
+			clear_image_summ = tf.summary.image("Out_Clear", self.clearImage)
+
+		with tf.name_scope("Loss") as scope:
+			clearloss = tf.losses.mean_squared_error(self.clear_in, self.clearImage)
+			maploss = tf.losses.mean_squared_error(self.trans_in, self.transMap)
+			self.loss = clearloss + maploss
+			self.train_loss_summ = tf.summary.scalar("Train_Loss", self.loss)
+			# self.val_loss_summ = tf.summary.scalar("Val_Loss", self.loss)
+							 
+		with tf.name_scope("Optimizers") as scope:
+			self.solver = tf.train.AdamOptimizer(learning_rate=1e-03).minimize(self.loss)
+
+		self.merged_summ = tf.summary.merge_all()
+		config = tf.ConfigProto()
+		config.gpu_options.allow_growth = True
+		self.sess = tf.Session(config=config)
+		self.train_writer = tf.summary.FileWriter(self.graph_path+'train/')
+		self.train_writer.add_graph(self.sess.graph)
+		self.val_writer = tf.summary.FileWriter(self.graph_path+'val/')
+		self.val_writer.add_graph(self.sess.graph)
+		self.saver = tf.train.Saver()
+		self.sess.run(tf.global_variables_initializer())
+		self._debug_info()
+
+	def train_model(self, train_imgs, val_imgs, learning_rate=1e-5, batch_size=32, epoch_size=50):
+		
+		print "Training Images: ", train_imgs[0].shape[0]
+		print "Validation Images: ", val_imgs[0].shape[0]
+		print "Learning_rate: ", learning_rate, "Batch_size", batch_size, "Epochs", epoch_size
+		raw_input("Training will start above configuration. Press Enter to Start....")
+		
+		with tf.name_scope("Training") as scope:
+			for epoch in range(epoch_size):
+				for itr in xrange(0, train_imgs[0].shape[0]-batch_size, batch_size):
+					haze_in = train_imgs[0][itr:itr+batch_size,0]
+					clear_in = train_imgs[0][itr:itr+batch_size,1]
+					trans_in = train_imgs[1][itr:itr+batch_size]
+
+					sess_in = [self.solver, self.loss, self.merged_summ]
+					sess_out = self.sess.run(sess_in, {self.haze_in:haze_in, self.clear_in: clear_in, self.trans_in: trans_in, self.train_phase:True})
+					self.train_writer.add_summary(sess_out[2])
+
+					if itr%5==0:
+						print "Epoch:", epoch, "Iteration:", itr/batch_size, "Loss:", sess_out[1] 
+
+						
+				for itr in xrange(0, val_imgs[0].shape[0]-batch_size, batch_size):
+					haze_in = val_imgs[0][itr:itr+batch_size,0]
+					clear_in = val_imgs[0][itr:itr+batch_size,1]
+					trans_in = val_imgs[1][itr:itr+batch_size]
+
+					loss ,summ = self.sess.run([self.loss, self.merged_summ], {self.haze_in: haze_in, 
+												self.clear_in: clear_in, self.trans_in:trans_in ,self.train_phase:False})
+					self.val_writer.add_summary(summ)
+
+					print "Epoch: ", epoch, "Iteration: ", itr/batch_size, "Validation Loss: ", loss
+
+				if epoch%10==0:
+					self.saver.save(self.sess, self.save_path+"AOD", global_step=epoch)
+					print "Checkpoint saved"
+
+					# a = np.random.randint(1, train_phaseimgs[0].shape[0], 1)
+					
+					# random_img = train_imgs[0][a]
+
+					# gen_imgs = self.sess.run(self.clearImg, {self.haze_in: random_img[:,0,:,:,:], self.trans_in: train_imgs[1][a], self.train_phase:False})
+					# for i,j,k in zip(random_img[:,0,:,:,:], random_img[:,1,:,:,:], gen_imgs):
+					# 	stack = np.hstack((i,j,k))
+					# 	cv2.imwrite(self.output_path +str(epoch)+"_train_img.jpg", 255.0*stack)
+
+	# def test(self, input_imgs, batch_size):
+	def test(self, batch_size):
+		sess=tf.Session()
+		
+		saver = tf.train.import_meta_graph(self.save_path+'AOD-50.meta')
+		print self.save_path
+		saver.restore(sess,tf.train.latest_checkpoint(self.save_path))
+		print self.save_path
+		graph = tf.get_default_graph()
+
+		x = graph.get_tensor_by_name("Inputs/Haze_Image:0")
+		is_train = graph.get_tensor_by_name("Inputs/is_training:0")
+		y = graph.get_tensor_by_name("Model/clearImage/Relu:0")
+
+		# print "Tensor Loaded"
+		# for itr in xrange(0, input_imgs.shape[0], batch_size):
+		# 	if itr+batch_size<=input_imgs.shape[0]:
+		# 		end = itr+batch_size
+		# 	else:
+		# 		end = input_imgs.shape[0]
+		# 	input_img = input_imgs[itr:end]
+		# 	out = sess.run(y, {x:input_img, is_train:False})
+		# 	if itr==0:
+		# 		tot_clr = out
+		# 	else:
+		# 		tot_clr = np.concatenate((tot_clr, out))
+		# print "Output Shape:", tot_clr.shape
+		# return tot_clr
+
+		img_name = glob.glob("/media/mnt/dehaze/*_resize.jpg")
+		for image in img_name:
+			img = cv2.imread(image)
+			in_img = img.reshape((1, img.shape[0],img.shape[1],img.shape[2]))
+			out = sess.run(y, {x:in_img/255.0, is_train:False})
+			# plt.imshow(out[2][0])
+			# plt.show()
+			# plt.imshow(out[3][0])
+			# plt.show()
+			# maps = sess.run(y, {x:in_img/255.0, is_train:False})
+			# clear = clearImg(img, maps[0])
+			# cv2.imwrite(image[:-4]+"_map.jpg", out[0][0]*255.0)
+			cv2.imwrite(image[:-4]+"_clear.jpg", out[0]*255.0)
+
+class pix2pix(object):
+	"""pix2pix from Image to Image translation"""
+	def __init__(self, model_path):
+		self.model_path = model_path
+		self.graph_path = model_path+"/tf_graph/"
+		self.save_path = model_path + "/saved_model/"
+		self.output_path = model_path + "/results/"
+		if not os.path.exists(model_path):
+			os.makedirs(self.graph_path+"train/")
+			os.makedirs(self.graph_path+"val/")
+		
+	def _debug_info(self):
+		variables_names = [[v.name, v.get_shape().as_list()] for v in tf.trainable_variables()]
+		print "Trainable Variables:"
+		tot_params = 0
+		for i in variables_names:
+			var_params = np.prod(np.array(i[1]))
+			tot_params += var_params
+			print i[0], i[1], var_params
+		print "Total number of Trainable Parameters: ", str(tot_params/1000.0)+"K"
+
+	def _generator(self, input_img):
+		with tf.variable_scope("generator") as scope:
+			with tf.variable_scope("encoder") as scope:
+				enc1 = conv_2d(input_img, output_chan=64, kernel=[4,4], stride=[2,2], use_bn=False, activation=leaky_relu,
+					train_phase=self.train_phase, name="enc1")
+				print "1",enc1.get_shape()
+				enc2 = conv_2d(enc1, output_chan=128, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu,
+					train_phase=self.train_phase, name="enc2")
+				print "2",enc2.get_shape()
+				enc3 = conv_2d(enc2, output_chan=256, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu,
+					train_phase=self.train_phase, name="enc3")
+				print "3",enc3.get_shape()
+				enc4 = conv_2d(enc3, output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True, activation=None,
+					train_phase=self.train_phase, name="enc4")
+				print "4",enc4.get_shape()
+				# enc5 = conv_2d(enc4, output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu,
+				# 	train_phase=self.train_phase, name="enc5")
+				# print "5",enc5.get_shape()
+				# enc6 = conv_2d(enc5, output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu,
+				# 	train_phase=self.train_phase, name="enc6")
+				# print "6",enc6.get_shape()
+			
+			with tf.variable_scope("decoder") as scope:
+				# dec6 = deconv_2d(enc6, output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True,
+				# 	train_phase=self.train_phase, name="dec6")
+				# dec5 = deconv_2d(tf.concat([dec6, enc5], 3), output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True,
+				# 	train_phase=self.train_phase, name="dec5")
+				# dec4 = deconv_2d(tf.concat([dec5, enc4], 3), output_chan=256, kernel=[4,4], stride=[2,2], use_bn=True,
+				# 	train_phase=self.train_phase, name="dec4")
+				dec4 = deconv_2d(enc4, output_chan=256, kernel=[4,4], stride=[2,2], use_bn=True,
+					train_phase=self.train_phase, name="dec4")
+				dec3 = deconv_2d(tf.concat([dec4, enc3], 3), output_chan=128, kernel=[4,4], stride=[2,2], use_bn=True,
+					train_phase=self.train_phase, name="dec3")
+				dec2 = deconv_2d(tf.concat([dec3, enc2], 3), output_chan=64, kernel=[4,4], stride=[2,2], use_bn=True,
+					train_phase=self.train_phase, name="dec2")
+				dec1 = deconv_2d(tf.concat([dec2, enc1], 3), output_chan=3, kernel=[4,4], stride=[2,2], use_bn=False,
+					activation=tf.tanh, train_phase=self.train_phase, name="dec1")
+
+		return dec1
+
+	def _discriminator(self, gen_input, gen_output, reuse):
+		with tf.variable_scope("discriminator", reuse=reuse) as scope:
+			conv1 = conv_2d(tf.concat([gen_input, gen_output],3), output_chan=64, kernel=[4,4], stride=[2,2], use_bn=False, activation=leaky_relu, 
+				train_phase=self.train_phase, name="conv1")
+			conv2 = conv_2d(conv1, output_chan=128, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu, 
+				train_phase=self.train_phase, name="conv2")
+			conv3 = conv_2d(conv2, output_chan=256, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu, 
+				train_phase=self.train_phase, name="conv3")
+			conv4 = conv_2d(conv3, output_chan=512, kernel=[4,4], stride=[2,2], use_bn=True, activation=leaky_relu, 
+				train_phase=self.train_phase, name="conv4")
+			conv5 = conv_2d(conv4, output_chan=1, kernel=[4,4], stride=[1,1], use_bn=False, activation=tf.sigmoid, 
+				train_phase=self.train_phase, name="conv5")
+
+		print conv5.get_shape()
+		return conv5
+
+	def build_model(self):
+		with tf.name_scope("Inputs") as scope:
+			self.haze_in = tf.placeholder(tf.float32, shape=[None,240,320,3], name="Haze_Image")
+			self.clear_in = tf.placeholder(tf.float32, shape=[None,240,320,3], name="Clear_Image")
+			# self.trans_in = tf.placeholder(tf.float32, shape=[None,240,320,1], name="Trans_Map")
+			self.train_phase = tf.placeholder(tf.bool, name="is_training")
+			hazy_summ = tf.summary.image("Hazy_image", (self.haze_in+1)/2)
+			clear_summ = tf.summary.image("clear_in", (self.clear_in+1)/2)
+
+		with tf.name_scope("Model") as scope:
+			self.gen_out = self._generator(self.haze_in)
+			self.dis_real = self._discriminator(self.haze_in, self.clear_in, reuse=False)
+			self.dis_fake = self._discriminator(self.haze_in, self.gen_out, reuse=True)
+			gen_out_summ = tf.summary.image("output_image", (self.gen_out+1)/2)
+
+		with tf.name_scope("Loss") as scope:
+			self.dis_loss = tf.reduce_mean(-(tf.log(self.dis_real + 1e-12) + tf.log(1 - self.dis_fake + 1e-12)))
+			self.gen_ad_loss = tf.reduce_mean(-tf.log(self.dis_fake + 1e-12))
+			self.gen_L1_loss = tf.reduce_mean(tf.abs(self.clear_in - self.gen_out))
+			self.gen_loss = self.gen_ad_loss+100.0*self.gen_L1_loss
+
+			dis_loss_summ = tf.summary.scalar("dis_loss", self.dis_loss)
+			gen_l1_summ = tf.summary.scalar("gen_l1", self.gen_L1_loss)
+			gen_ad_summ = tf.summary.scalar("gen_ad_loss", self.gen_ad_loss)
+							 
+		with tf.name_scope("Optimizers") as scope:
+			dis_var_list = [var for var in tf.trainable_variables() if "discriminator" in var.name]
+			gen_var_list = [var for var in tf.trainable_variables() if "generator" in var.name]
+			print "Generator"
+			for var in gen_var_list:
+				print var.name
+			print "discriminator"
+			for var in dis_var_list:
+				print var.name
+
+			self.dis_solver = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5).minimize(self.dis_loss, var_list=dis_var_list)
+
+			update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+  			with tf.control_dependencies(update_ops):
+				self.gen_solver = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5).minimize(self.gen_loss, var_list=gen_var_list)
+
+			self.gen_summ = tf.summary.merge([hazy_summ, clear_summ, gen_out_summ, gen_l1_summ, gen_ad_summ])
+			self.dis_summ = tf.summary.merge([dis_loss_summ])
+
+		config = tf.ConfigProto()
+		config.gpu_options.allow_growth = True
+		self.sess = tf.Session(config=config)
+		self.train_writer = tf.summary.FileWriter(self.graph_path+'train/')
+		self.train_writer.add_graph(self.sess.graph)
+		self.val_writer = tf.summary.FileWriter(self.graph_path+'val/')
+		self.val_writer.add_graph(self.sess.graph)
+		self.saver = tf.train.Saver()
+		self.sess.run(tf.global_variables_initializer())
+		self._debug_info()
+
+	def train_model(self, train_imgs, val_imgs, learning_rate=1e-5, batch_size=32, epoch_size=50):
+		
+		print "Training Images: ", train_imgs.shape[0] 
+		print "Validation Images: ", val_imgs.shape[0]
+		# print "Training Images: ", train_imgs[0].shape[0] 
+		# print "Validation Images: ", val_imgs[0].shape[0]
+		print "Learning_rate: ", learning_rate, "Batch_size", batch_size, "Epochs", epoch_size
+		raw_input("Training will start above configuration. Press Enter to Start....")
+		
+		count = 0
+		with tf.name_scope("Training") as scope:
+			for epoch in range(epoch_size):
+				for itr in xrange(0, train_imgs.shape[0]-batch_size, batch_size):
+					# haze_in = train_imgs[0][itr:itr+batch_size,0]
+					# clear_in = train_imgs[0][itr:itr+batch_size,1]
+					# trans_in = train_imgs[1][itr:itr+batch_size]
+						
+					haze_in = train_imgs[itr:itr+batch_size,0]
+					clear_in = train_imgs[itr:itr+batch_size,1]
+
+					dis_in = [self.dis_solver, self.dis_loss, self.dis_summ]
+					dis_out = self.sess.run(dis_in, {self.haze_in:haze_in, self.clear_in: clear_in, self.train_phase:True})
+
+					sess_in = [self.gen_solver, self.gen_ad_loss, self.gen_L1_loss, self.gen_summ]
+					gen_out = self.sess.run(sess_in, {self.haze_in:haze_in, self.clear_in: clear_in, self.train_phase:True})
+					
+					if itr%5==0:
+						print "Epoch:", epoch, "Iteration:", itr/batch_size, "Dis Loss:", dis_out[1], "Gen L1:", gen_out[2], \
+						"Gen adver:", gen_out[1]
+					 
+					self.train_writer.add_summary(dis_out[2], count)
+					self.train_writer.add_summary(gen_out[3], count)
+					count = count +1
+					
+				for itr in xrange(0, val_imgs.shape[0]-batch_size, batch_size):
+					haze_in = val_imgs[itr:itr+batch_size,0]
+					clear_in = val_imgs[itr:itr+batch_size,1]
+					# trans_in = val_imgs[1][itr:itr+batch_size]
+
+					sess_in = [self.dis_loss, self.gen_ad_loss, self.gen_L1_loss,self.dis_summ, self.gen_summ]
+					sess_out = self.sess.run(sess_in, {self.haze_in: haze_in, 
+												self.clear_in: clear_in,self.train_phase:False})
+					self.val_writer.add_summary(sess_out[3], count)
+					self.val_writer.add_summary(sess_out[4], count)
+
+					print "Validation Epoch:", epoch, "Iteration:", itr/batch_size, "Dis Loss:", sess_out[0], "Gen L1:", sess_out[2], \
+						"Gen adver:", sess_out[1]
+
+				if epoch%10==0:
+					self.saver.save(self.sess, self.save_path+"pix2pix", global_step=epoch)
+					print "Checkpoint saved"
+
+					# a = np.random.randint(1, train_phaseimgs[0].shape[0], 1)
+					
+					# random_img = train_imgs[0][a]
+
+					# gen_imgs = self.sess.run(self.clearImg, {self.haze_in: random_img[:,0,:,:,:], self.trans_in: train_imgs[1][a], self.train_phase:False})
+					# for i,j,k in zip(random_img[:,0,:,:,:], random_img[:,1,:,:,:], gen_imgs):
+					# 	stack = np.hstack((i,j,k))
+					# 	cv2.imwrite(self.output_path +str(epoch)+"_train_img.jpg", 255.0*stack)
+
+	# def test(self, batch_size):
+	def test(self, input_imgs, batch_size):
+		sess=tf.Session()
+		
+		saver = tf.train.import_meta_graph(self.save_path+'pix2pix-120.meta')
+		print self.save_path
+		saver.restore(sess,tf.train.latest_checkpoint(self.save_path))
+		print self.save_path
+		graph = tf.get_default_graph()
+
+		x = graph.get_tensor_by_name("Inputs/Haze_Image:0")
+		is_train = graph.get_tensor_by_name("Inputs/is_training:0")
+		y = graph.get_tensor_by_name("Model/generator/decoder/dec1/Tanh:0")
+
+		print "Tensor Loaded"
+		for itr in xrange(0, input_imgs.shape[0], batch_size):
+			if itr+batch_size<=input_imgs.shape[0]:
+				end = itr+batch_size
+			else:
+				end = input_imgs.shape[0]
+			input_img = input_imgs[itr:end]
+			out = sess.run(y, {x:input_img/127.5-1, is_train:False})
+			if itr==0:
+				tot_clr = out
+			else:
+				tot_clr = np.concatenate((tot_clr, out))
+		print "Output Shape:", tot_clr.shape
+		return (tot_clr+1)/2
+
+		# img_name = glob.glob("/media/mnt/dehaze/*_resize.jpg")
+		# for image in img_name:
+		# 	img = cv2.imread(image)
+		# 	in_img = img.reshape((1, img.shape[0],img.shape[1],img.shape[2]))
+		# 	out = sess.run(y, {x:in_img/255.0, is_train:False})
+		# 	# plt.imshow(out[2][0])
+		# 	# plt.show()
+		# 	# plt.imshow(out[3][0])
+		# 	# plt.show()
+		# 	# maps = sess.run(y, {x:in_img/255.0, is_train:False})
+		# 	# clear = clearImg(img, maps[0])
+		# 	# cv2.imwrite(image[:-4]+"_map.jpg", out[0][0]*255.0)
+		
